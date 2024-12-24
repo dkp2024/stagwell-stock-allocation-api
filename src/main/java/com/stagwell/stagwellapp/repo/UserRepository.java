7c077e36f39269a0aa4b86ed14c858bf4f0abcce
Dharmendra Kumar Prajapati <v-dharmendra.kumar.prajapati@stagwell.onmicrosoft.com>
2024-12-24 21:10:49 +0530

    Method parameters are corrected

--- src/main/java/com/stagwell/stagwellapp/repo/UserRepository.java
+++ src/main/java/com/stagwell/stagwellapp/repo/UserRepository.java
@@ -39,36 +39,36 @@ extends JpaRepository<SwmUserInfo, Integer> {
 
     @Modifying
     @Query(nativeQuery=true, value="INSERT INTO public.swt_special_requests(\n\tagency_id, cfo_name, job_title, additional_cash_bonus_request, additional_stock_bonus_request, message_to_approver, approval_status, created_by, updated_dt)\n\tVALUES (:#{#sr.agencyId}, :#{#sr.cfoName}, :#{#sr.jobTitle}, :#{#sr.additionalCashBonusRequest}, :#{#sr.additionalStockBonusRequest}, :#{#sr.messageToApprover}, :#{#sr.approvalStatus}, :#{#sr.createdBy}, :#{#sr.updatedDate})")
-    public void saveSwtSpecialRequests(SpecialRequestDto var1);
+    public void saveSwtSpecialRequests(SpecialRequestDto sr);
 
     @Query(value="SELECT special_request_id, agency_id, cfo_name, job_title, additional_cash_bonus_request, additional_stock_bonus_request, message_to_approver, approval_status, created_by, updated_dt\n\tFROM swt_special_requests WHERE agency_id=:agencyId ORDER BY special_request_id DESC LIMIT 5", nativeQuery=true)
-    public List<Map<String, Object>> getSpecialRequests(int var1);
+    public List<Map<String, Object>> getSpecialRequests(int agencyId);
 
     @Modifying
     @Query(nativeQuery=true, value="UPDATE swt_entity_budget_allocation SET amount_allocated_to_cash=:#{#ue.amountForCash}, amount_allocated_to_stock=:#{#ue.amountForStock}, status=:#{#ue.status} WHERE entity_id=:#{#ue.entityId}")
-    public void saveEntity(UserEntityDto var1);
+    public void saveEntity(UserEntityDto ue);
 
     @Modifying
     @Query(nativeQuery=true, value="UPDATE swt_special_requests SET \napproved_cash_bonus=:#{#sr.approvedCashBonus}, approved_stock_bonus=:#{#sr.approvedStockBonus},\napproval_status=:#{#sr.approvalStatus} WHERE special_request_id=:#{#sr.specialRequestId}")
-    public void updateSpecialRequestWithApprovedSRValues(SpecialRequestApproveDto var1);
+    public void updateSpecialRequestWithApprovedSRValues(SpecialRequestApproveDto sr);
 
     @Modifying
     @Query(nativeQuery=true, value="UPDATE swt_special_requests SET \napproval_status=:#{#sr.approvalStatus} WHERE special_request_id=:#{#sr.specialRequestId}")
-    public void updateSpecialRequestWithoutApprovedSRValues(SpecialRequestApproveDto var1);
+    public void updateSpecialRequestWithoutApprovedSRValues(SpecialRequestApproveDto sr);
 
     @Modifying
     @Query(nativeQuery=true, value="UPDATE swt_agency_budget_allocation AS a SET \namount_allocated_to_cash=amount_allocated_to_cash+:cashAmount, amount_allocated_to_stock=amount_allocated_to_stock+:stockAmount \nFROM swm_agency_info AS b WHERE a.agency_id=b.agency_id AND b.agency_id=:agencyId")
-    public void incrementAgencyAmount(int var1, int var2, int var3);
+    public void incrementAgencyAmount(int cashAmount, int stockAmount, int agencyId);
 
     @Modifying
     @Query(nativeQuery=true, value="UPDATE swt_entity_budget_allocation SET \namount_allocated_to_cash=amount_allocated_to_cash+:cashAmount, amount_allocated_to_stock=amount_allocated_to_stock+:stockAmount \nWHERE entity_id=:entityId")
-    public void incrementEntityAmount(int var1, int var2, int var3);
+    public void incrementEntityAmount(int cashAmount, int stockAmount, int entityId);
 
     @Modifying
     @Query(nativeQuery=true, value="UPDATE swt_entity_budget_allocation SET current_stage=:stage WHERE entity_id=:entityId")
-    public void updateStage(String var1, int var2);
+    public void updateStage(String stage, int entityId);
 
     @Modifying
     @Query(nativeQuery=true, value="SELECT entity_id FROM swm_entity_info WHERE agency_id=:agencyId")
-    public List<Map<String, Object>> getEntitiesByAgencyId(int var1);
+    public List<Map<String, Object>> getEntitiesByAgencyId(int agencyId);
 }
