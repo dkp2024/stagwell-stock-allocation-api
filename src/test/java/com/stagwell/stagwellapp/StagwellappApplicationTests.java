39bb5eb668a903e06bc1811ee294675d87facd6e
Dharmendra Kumar Prajapati <v-dharmendra.kumar.prajapati@stagwell.onmicrosoft.com>
2024-12-31 12:42:08 +0530

    app name changed

--- src/test/java/com/stagwell/stagwellapp/StagwellappApplicationTests.java
+++ src/test/java/com/stagwell/stagwellapp/StagwellappApplicationTests.java
@@ -3,10 +3,10 @@ package com.stagwell.stagwellapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class StagwellappApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
